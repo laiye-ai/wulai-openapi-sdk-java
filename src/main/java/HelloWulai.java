ce10e8b256b116f2cb9decef3534ed0e9091389e
zhangtaolaiye <zhangtao@laiye.com>
2019-08-02 12:19:29 +0800

    删除敏感信息

--- src/main/java/HelloWulai.java
+++
@@ -1,9 +0,0 @@
-public class HelloWulai {
-
-    public static void main(String[] args) {
-        Wulai wulai=Wulai.create("84Mb38jJR6Bg7qB3MT09cSHPdrEImIgv00362b370af3cc02eb",
-                "x9xBAnz8JHrI8GdsdVoJ","https://openapi.wul.ai");
-
-        wulai.
-    }
-}
