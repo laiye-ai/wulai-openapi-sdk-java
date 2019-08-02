7f895169664efec5eb738f60d7057b2149fdfde2
zhangtaolaiye <zhangtao@laiye.com>
2019-08-02 11:53:17 +0800

    rm App.java

--- src/App.java
+++
@@ -1,7 +0,0 @@
-
-
-class App{
-    public static void main(String[] args) {
-        System.out.println("hello world");
-    }
-}
--- src/main/java/HelloWulai.java
+++ src/main/java/HelloWulai.java
@@ -1,2 +1,9 @@
 public class HelloWulai {
+
+    public static void main(String[] args) {
+        Wulai wulai=Wulai.create("84Mb38jJR6Bg7qB3MT09cSHPdrEImIgv00362b370af3cc02eb",
+                "x9xBAnz8JHrI8GdsdVoJ","https://openapi.wul.ai");
+
+        wulai.
+    }
 }
