4e0572977fb920a1d91e4bb4cc0f487c7de6c686
zhangtaolaiye <zhangtao@laiye.com>
2019-08-06 10:23:55 +0800

    测试构造方法

--- src/main/java/exceptions/Server_Exception.java
+++ src/main/java/exceptions/Server_Exception.java
@@ -1,4 +1,15 @@
 package exceptions;
 
-public class Server_Exception {
+public class Server_Exception extends Exception {
+    static String http_code;
+    static String error_code;
+    static String error_message;
+    public Server_Exception(String http_code, String error_code, String error_message) {
+        this.http_code=http_code;
+        this.error_code=http_code;
+        this.error_message=error_message;
+    }
+
+    private Server_Exception() {
+    }
 }
