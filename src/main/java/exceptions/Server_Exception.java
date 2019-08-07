58733b9d9ecd3fcb597dec06c25468127a898453
zhangtaolaiye <zhangtao@laiye.com>
2019-08-07 15:08:05 +0800

    add exceptionClass

--- src/main/java/exceptions/Server_Exception.java
+++ src/main/java/exceptions/Server_Exception.java
@@ -1,15 +1,22 @@
 package exceptions;
 
-public class Server_Exception extends Exception {
-    static String http_code;
-    static String error_code;
-    static String error_message;
-    public Server_Exception(String http_code, String error_code, String error_message) {
-        this.http_code=http_code;
-        this.error_code=http_code;
-        this.error_message=error_message;
+public class Server_Exception extends Client_Exception {
+    private static final long serialVersionUID = 7025739208436927199L;
+    private int http_code;
+
+    public Server_Exception(String error_code, String error_message, int http_code) {
+        super(error_code, error_message);
+        setHttp_code(http_code);
+        this.setErrorType(ErrorType.Server);
     }
 
-    private Server_Exception() {
+    public int getHttp_code() {
+        return http_code;
     }
+
+    public void setHttp_code(int http_code) {
+        this.http_code = http_code;
+    }
+
+
 }
