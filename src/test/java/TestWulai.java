b80b2271db162d5693b946ae6a069136654d3726
zhangtao <zhangtao@laiye.com>
2019-08-20 17:13:28 +0800

    Update TestWulai.java

--- src/test/java/TestWulai.java
+++ src/test/java/TestWulai.java
@@ -19,14 +19,14 @@ public class TestWulai  {
 
     @Test
     public void testwulai() throws ClientException, IOException {
-        WulaiClient client1 = WulaiClient.getInstance("NMZXkOobOmLBbZdPoeERyQ53qXWyySRt006e1805f2230492a8", "U0xdSUh9PbW5jx8Qf1vk", "v1", false);
+        WulaiClient client1 = WulaiClient.getInstance(System.getenv("pubkeyv1"), System.getenv("secretv1"), "v1", false);
 
         String data = String.format("{\"user_id\":\"%s\",\"msg_body\":{\"text\":{\"content\":\"%s\"}},\"extra\":\"%s\"}", name, "你是谁", "");
         Long time1=System.currentTimeMillis();
 
         for (int i=0;i<20;i++){
-            WulaiClient client2 = WulaiClient.getInstance("84Mb38jJR6Bg7qB3MT09cSHPdrEImIgv00362b370af3cc02eb",
-                    "x9xBAnz8JHrI8GdsdVoJ", "v2", true);
+            WulaiClient client2 = WulaiClient.getInstance(System.getenv("pubkey"),
+                    System.getenv("secret"), "v2", true);
             client2.processCommonRequest("/msg/bot-response", data, "post");
             System.err.println(i);
         }
