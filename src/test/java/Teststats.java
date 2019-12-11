1d2e20e98a03bbf3487bc5dae24d72296333b186
zhangtao@laiye.com <zhangtao@laiye.com>
2019-12-11 12:13:57 +0800

    hell

--- src/test/java/Teststats.java
+++ src/test/java/Teststats.java
@@ -1,6 +1,10 @@
 import com.DefaultClient;
 import com.exceptions.ClientException;
 import com.exceptions.ServerException;
+import com.module.request.MsgBody;
+import com.module.request.Text;
+import com.module.response.msg.BotResponse;
+import com.wulai.msg.GetBotResponse;
 import com.wulai.stats.QuerySatisfactionDailyKnowledgeList;
 import com.wulai.stats.QueryQaRecallDailyList;
 import org.junit.Test;
@@ -76,7 +80,29 @@ public class Teststats {
 
 
     }
+    @Test
+    public void TestCreateSatisfaction(){
+        Text text=new Text("怀孕适合去哪里旅游");
+        MsgBody msgBody=new MsgBody(text);
 
+        GetBotResponse getBotResponse=new GetBotResponse();
+        getBotResponse.setUserId("zhangtao@test");
+        getBotResponse.setMsgBody(msgBody);
+        BotResponse botResponse=null;
+        try {
+            botResponse=getBotResponse.request(defaultClient);
+        }catch (ServerException|ClientException e){
+            e.printStackTrace();
+        }
+
+        for (Object o:botResponse.getSuggestedResponse()
+        ) {
+            System.out.println(o);
+        }
+
+        System.out.println("hello");
+
+    }
 
 
 
