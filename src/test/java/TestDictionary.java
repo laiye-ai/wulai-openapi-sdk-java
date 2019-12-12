98ed2f11d0b15488cb4f1be2de4c5f48a1d088be
zhangtao@laiye.com <zhangtao@laiye.com>
2019-12-12 16:41:14 +0800

    add Test

--- src/test/java/TestDictionary.java
+++ src/test/java/TestDictionary.java
@@ -1,4 +1,171 @@
-package PACKAGE_NAME;
+import com.DefaultClient;
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONArray;
+import com.alibaba.fastjson.JSONObject;
+import com.exceptions.ClientException;
+import com.exceptions.ServerException;
+import com.module.request.dictionary.*;
+import com.wulai.dictionary.*;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 
 public class TestDictionary {
+
+    DefaultClient defaultClient = new DefaultClient();
+
+    //实体类接口测试*(9)
+    @Test
+    public void TestEntity() throws ServerException, ClientException {
+
+        //1.查询全部实体概要
+        QueryEntityList queryEntityList = new QueryEntityList();
+        queryEntityList.setPage(1);
+        queryEntityList.setPageSize(100);
+        Map map = queryEntityList.request(defaultClient);
+
+        for (Object o : map.keySet()
+        ) {
+            System.out.println(o);
+            System.out.println(map.get(o));
+
+        }
+        JSONArray jsonArray = JSON.parseArray(map.get("entities").toString());
+        System.out.println(jsonArray.size());
+
+        //2.获取实体详情
+        GetEntity getEntity = new GetEntity();
+        getEntity.setId(39750);
+        Map getentity = getEntity.request(defaultClient);
+        System.out.println(getentity);
+
+        //3.创建枚举实体
+        EnumEntity enumEntity = new EnumEntity();
+        enumEntity.setName("大屏手机");
+
+        CreateEntityEnumeration createEntityEnumeration = new CreateEntityEnumeration();
+        createEntityEnumeration.setEnumEntity(enumEntity);
+        Map createEntityEnumerationMap = createEntityEnumeration.request(defaultClient);
+        System.out.println(createEntityEnumerationMap);
+        Object o=createEntityEnumerationMap.get("enum_entity");
+        JSONObject json=JSONObject.parseObject(o.toString());
+        int entityId=Integer.valueOf(json.get("id").toString());
+        //4.创建枚举实体值
+        CreateEntityEnumerationValue createEntityEnumerationValue=new CreateEntityEnumerationValue();
+        createEntityEnumerationValue.setEntityId(entityId);
+        Value value=new Value();
+        value.setStandard_value("智能手机");
+        value.addSynonym("苹果");
+        value.addSynonym("华为");
+        value.addSynonym("三星");
+        value.addSynonym("小米");
+        createEntityEnumerationValue.setValue(value);
+        Map createEntityEnumerationValueMap=createEntityEnumerationValue.request(defaultClient);
+        System.out.println(createEntityEnumerationValueMap);
+
+        //5.删除枚举实体值
+        DeleteEntityEnumerationValue deleteEntityEnumerationValue=new DeleteEntityEnumerationValue();
+        deleteEntityEnumerationValue.setEntityId(entityId);
+        List synonyms=value.getSynonyms();
+        synonyms.remove(1);
+        synonyms.remove(1);
+        value.setSynonyms(synonyms);
+        deleteEntityEnumerationValue.setValue(value);
+        deleteEntityEnumerationValue.request(defaultClient);
+
+
+        //6.删除实体
+        DeleteEntity deleteEntity=new DeleteEntity();
+        deleteEntity.setId(entityId);
+        int code=deleteEntity.request(defaultClient);
+        if (code==200){
+            System.out.println("delete enum success");
+        }
+
+        //7.创建意图实体
+
+
+        CreateEntityIntent createEntityIntent=new CreateEntityIntent();
+        IntentEntity intentEntity=new IntentEntity();
+        intentEntity.setName("肯定答复");
+        intentEntity.setStandard_value("嗯嗯");
+        createEntityIntent.setIntentEntity(intentEntity);
+        Map intentMap=createEntityIntent.request(defaultClient);
+
+        Object intentObj=intentMap.get("intent_entity");
+        JSONObject intentjson=JSONObject.parseObject(intentObj.toString());
+        int intentId=Integer.valueOf(intentjson.get("id").toString());
+
+        //8.创建意图实体值相似说法
+        CreateEntityIntentValue createEntityIntentValue=new CreateEntityIntentValue();
+        createEntityIntentValue.setEntityId(intentId);
+        createEntityIntentValue.addSynonyms("好的");
+        createEntityIntentValue.addSynonyms("可以");
+        createEntityIntentValue.addSynonyms("牛逼");
+        createEntityIntentValue.addSynonyms("OJBK");
+        Map intentValueMap=createEntityIntentValue.request(defaultClient);
+        System.out.println(intentValueMap);
+
+        //9.删除意图实体值相似说法
+        DeleteEntityIntentValue deleteEntityIntentValue=new DeleteEntityIntentValue();
+        deleteEntityIntentValue.setEntityId(intentId);
+        deleteEntityIntentValue.addSynonym("可以");
+        deleteEntityIntentValue.request(defaultClient);
+
+        //删除实体
+        deleteEntity.setId(intentId);
+        int intentcode=deleteEntity.request(defaultClient);
+        if (intentcode==200){
+            System.out.println("delete intentEntity success");
+        }
+    }
+
+    //专有词汇接口测试（4）
+    @Test
+    public void TestTerm() throws ServerException,ClientException{
+        //1.创建专有词汇
+        CreateTerm createTerm=new CreateTerm();
+        Term term=new Term();
+        term.setName("CEO");
+        TermItem termItem =new TermItem();
+        termItem.setTerm(term);
+        createTerm.setTermItem(termItem);
+        Map createTermMap=createTerm.request(defaultClient);
+        System.out.println(createTermMap);
+        String term_item=createTermMap.get("term_item").toString();
+        JSONObject jsonObject = JSONObject.parseObject(term_item);
+        String termstr=jsonObject.get("term").toString();
+        JSONObject json=JSONObject.parseObject(termstr);
+        String termId = json.get("id").toString();
+        //2.更新专有词汇
+        term.setId(Integer.valueOf(termId));
+        termItem.setTerm(term);
+        ArrayList<String> synonyms=new ArrayList<>();
+        synonyms.add("老板");
+        synonyms.add("老大");
+        termItem.setSynonyms(synonyms);
+        UpdateTerm updateTerm =new UpdateTerm();
+        updateTerm.setTermItem(termItem);
+
+        Map map1=updateTerm.request(defaultClient);
+        System.out.println(map1);
+
+        //3.删除专有词汇
+        DeleteTerm deleteTerm=new DeleteTerm();
+        deleteTerm.setId(termId);
+        int code =deleteTerm.request(defaultClient);
+        if (code==200){
+            System.out.println("delete term ok");
+        }
+        //4.查询专有词汇列表
+        QueryTermList queryTermList=new QueryTermList();
+        queryTermList.setPage(1);
+        queryTermList.setPageSize(20);
+        Map map = queryTermList.request(defaultClient);
+        System.out.println(map);
+    }
+
+
 }
--- src/test/java/TestKnowledge.java
+++ src/test/java/TestKnowledge.java
@@ -1,4 +1,284 @@
-package PACKAGE_NAME;
+import com.DefaultClient;
+import com.alibaba.fastjson.JSONObject;
+import com.exceptions.ClientException;
+import com.exceptions.ServerException;
+import com.module.request.*;
+import com.module.request.knowledge.*;
+import com.module.request.user.UserAttribute;
+import com.module.request.user.UserAttributeUserAttributeValue;
+import com.module.request.user.UserAttributeValue;
+import com.wulai.knowledge.*;
+import org.junit.Test;
+import java.util.Map;
+
 
 public class TestKnowledge {
+
+    DefaultClient defaultClient=new DefaultClient();
+
+    //相似问接口测试(4)
+    @Test
+    public void TestCreateSimilarQuestionList() throws ServerException,ClientException{
+
+        System.out.println("相似问接口测试。。。");
+
+        //查询相似问
+        QuerySimilarQuestionList querySimilarQuestionList=new QuerySimilarQuestionList();
+        querySimilarQuestionList.setPage(1);
+        querySimilarQuestionList.setPageSize(3);
+        querySimilarQuestionList.setKnowledgeId("991640");
+
+        Map map4 = querySimilarQuestionList.request(defaultClient);
+        int i =0;
+        for (Object o :map4.keySet()
+        ) {
+            System.out.println(o);
+            System.out.println(map4.get(o));
+        }
+
+
+        //创建相似问
+        CreateSimilarQuestion createSimilarQuestion=new CreateSimilarQuestion();
+        SimilarQuestion similarQuestion=new SimilarQuestion();
+        similarQuestion.setKnowledge_id("1583014");
+        similarQuestion.setQuestion("nihaowa");
+        createSimilarQuestion.setSimilarQuestion(similarQuestion);
+        Map map= createSimilarQuestion.request(defaultClient);
+        for (Object o :map.keySet()
+        ) {
+            System.out.println(o);
+            System.out.println(map.get(o));
+        }
+        JSONObject json=(JSONObject) JSONObject.parse(map.get("similar_question").toString());
+
+        similarQuestion.setId(json.get("id").toString());
+        similarQuestion.setQuestion("buhaowa");
+
+        //修改相似问
+        UpdateSimilarQuestion updateSimilarQuestion=new UpdateSimilarQuestion();
+        updateSimilarQuestion.setSimilarQuestion(similarQuestion);
+        Map map1= updateSimilarQuestion.request(defaultClient);
+        for (Object o1 :map1.keySet()
+        ) {
+            System.out.println(o1);
+            System.out.println(map.get(o1));
+        }
+
+        //删除相似问
+        DeleteSimilarQuestion deleteSimilarQuestion=new DeleteSimilarQuestion();
+        deleteSimilarQuestion.setId(json.get("id").toString());
+        int code=deleteSimilarQuestion.request(defaultClient);
+        System.out.println(code);
+
+        System.out.println("相似问接口测试完成");
+    }
+
+
+    //知识点接口测试(5)
+    @Test
+    public void TestKnowledgeTagKnowledge() throws ServerException,ClientException{
+        System.out.println("知识点接口测试。。。");
+        //1.查询知识点分类列表
+        QueryKnowledgeTagsList queryKnowledgeTagsList=new QueryKnowledgeTagsList();
+        queryKnowledgeTagsList.setPage(1);
+        queryKnowledgeTagsList.setPageSize(5);
+        queryKnowledgeTagsList.setParentKTagId("116469");
+        Map map6=null;
+        try {
+            map6=queryKnowledgeTagsList.request(defaultClient);
+        }catch (ServerException|ClientException e){
+
+            e.printStackTrace();
+        }
+        for (Object o :map6.keySet()
+        ) {
+            System.out.println(map6.get(o).toString());
+        }
+
+
+        //2.查询知识点
+        QueryKnowledgeItemsList queryKnowledgeItemsList=new QueryKnowledgeItemsList();
+        queryKnowledgeItemsList.setPage(1);
+        queryKnowledgeItemsList.setPageSize(200);
+
+
+        Map map5 =queryKnowledgeItemsList.request(defaultClient);
+        for (Object p:map5.keySet()
+        ) {
+            System.out.println(map5.get(p));
+        }
+
+
+        Knowledge knowledge=new Knowledge();
+        knowledge.setStatus(true);
+        knowledge.setRespond_all(true);
+        knowledge.setStandard_question("测试默认知识点接口813");
+        knowledge.setMaintained_by_user_attribute_group(false);
+        KnowledgeTagKnowledge knowledgeTagKnowledge=new KnowledgeTagKnowledge();
+        knowledgeTagKnowledge.setKnowledge(knowledge);
+        knowledgeTagKnowledge.setKnowledge_tag_id("116470");
+
+        //3.创建知识点
+        CreateKnowledgeTagKnowledge createKnowledgeTagKnowledge=new CreateKnowledgeTagKnowledge();
+        createKnowledgeTagKnowledge.setKnowledgeTagKnowledge(knowledgeTagKnowledge);
+        Map map=null;
+
+        map=createKnowledgeTagKnowledge.request(defaultClient);
+        for (Object o:map.keySet()
+             ) {
+            System.out.println(o);
+            System.out.println(map.get(o));
+        }
+        JSONObject jsonObject = (JSONObject) JSONObject.parse(map.get("knowledge_tag_knowledge").toString());
+        JSONObject jsonObject1 =  JSONObject.parseObject(jsonObject.get("knowledge").toString());
+        String id =jsonObject1.get("id").toString();
+
+        //4.更新知识点
+        knowledge.setRespond_all(false);
+        knowledge.setStandard_question("测试默认知识点接口833");
+        knowledge.setId(id);
+        UpdateKnowledge updateKnowledge=new UpdateKnowledge();
+        updateKnowledge.setKnowledge(knowledge);
+        Map map1 =updateKnowledge.request(defaultClient);
+        System.out.println("hh"+map1.values());
+
+
+        //5.删除知识点
+        DeleteKnowledge deleteKnowledge =new DeleteKnowledge();
+        deleteKnowledge.setId(Integer.valueOf(id));
+        int i =deleteKnowledge.request(defaultClient);
+        System.out.println(i);
+
+        System.out.println("知识点接口测试完成");
+    }
+
+
+    //用户属性组接口测试(3)
+    @Test
+    public void TestUserAttributeGroupItems() throws ServerException ,ClientException{
+        System.out.println("用户属性组接口测试");
+        //1.查询用户属性组列表
+        QueryUserAttributeGroupItemsList queryUserAttributeGroupItemsList=new QueryUserAttributeGroupItemsList();
+        queryUserAttributeGroupItemsList.setPageSize(20);
+        queryUserAttributeGroupItemsList.setPage(1);
+        Map map=queryUserAttributeGroupItemsList.request(defaultClient);
+        for (Object o : map.keySet()
+             ) {
+            System.out.println(map.get(o));
+        }
+
+        //2.创建用户属性组
+            //创建用户属性id
+        UserAttribute userAttribute=new UserAttribute("101945");
+            //创建用户属性值
+        UserAttributeValue userAttributeValue=new UserAttributeValue("测试测试");
+            //创建用户属性组对象
+        UserAttributeUserAttributeValue userAttributeUserAttributeValue=new UserAttributeUserAttributeValue();
+
+        userAttributeUserAttributeValue.setUser_attribute(userAttribute);
+        userAttributeUserAttributeValue.setUser_attribute_value(userAttributeValue);
+
+
+        UserAttributeGroupItem userAttributeGroupItem=new UserAttributeGroupItem();
+        UserAttributeGroup userAttributeGroup=new UserAttributeGroup();
+        userAttributeGroup.setName("接口测试");
+
+        userAttributeGroupItem.setUser_attribute_group(userAttributeGroup);
+        userAttributeGroupItem.addUserAttributeUserAttributeValue(userAttributeUserAttributeValue);
+
+        CreateUserAttributeGroupItems createUserAttributeGroupItems=new CreateUserAttributeGroupItems();
+        createUserAttributeGroupItems.setUserattributeGroupItem(userAttributeGroupItem);
+
+        Map map1=createUserAttributeGroupItems.request(defaultClient);
+
+        for (Object o : map1.keySet()
+        ) {
+            System.out.println(map1.get(o));
+        }
+
+        //3.更新属性组
+        UpdateUserAttributeGroupItems updateUserAttributeGroupItems=new UpdateUserAttributeGroupItems();
+        updateUserAttributeGroupItems.setUser_attribute_group_item(userAttributeGroupItem);
+        Map map2=updateUserAttributeGroupItems.request(defaultClient);
+        for (Object o:map2.keySet()
+             ) {
+            System.out.println(map.get(o));
+        }
+
+    }
+
+    //属性组回复接口(4)
+    @Test
+    public void TestQueryUserAttributeGroupAnswersList() throws  ServerException,ClientException{
+
+        //1.查询属性组回复列表
+        QueryUserAttributeGroupAnswersList queryUserAttributeGroupAnswersList=new QueryUserAttributeGroupAnswersList();
+        queryUserAttributeGroupAnswersList.setPageSize(1);
+        queryUserAttributeGroupAnswersList.setPage(1);
+        Filter filter =new Filter();
+        filter.setKnowledge_id("990308");
+        filter.setUser_attribute_group_id("0");
+        queryUserAttributeGroupAnswersList.setFilter(filter);
+        Map map=queryUserAttributeGroupAnswersList.request(defaultClient);
+        System.out.println("查询属性组回复列表");
+        for (Object o :map.keySet()
+             ) {
+
+            System.out.println(o);
+            System.out.println(map.get(o));
+        }
+
+
+        //2.创建属性组回复
+        Text text=new Text("测试回复");
+        MsgBody msgBody=new MsgBody(text);
+        Answer answer=new Answer();
+        answer.setId("3482630");
+        answer.setMsg_body(msgBody);
+        answer.setKnowledge_id("990308");
+
+        UserAttributeGroupAnswer userAttributeGroupAnswer=new UserAttributeGroupAnswer();
+        userAttributeGroupAnswer.setUser_attribute_group_id("0");
+        userAttributeGroupAnswer.setAnswer(answer);
+
+
+        CreateUserAttributeGroupAnswer createUserAttributeGroupAnswer=new CreateUserAttributeGroupAnswer();
+        createUserAttributeGroupAnswer.setUserAttributeGroupAnswer(userAttributeGroupAnswer);
+
+//        Map map1=createUserAttributeGroupAnswer.request(defaultClient);
+//        System.out.println("创建属性组回复");
+//        for (Object o :map1.keySet()
+//             ) {
+//            System.out.println(map1.get(o));
+//        }
+
+
+        //3.更新属性组回复
+        UpdateUserAttributeGroupAnswer updateUserAttributeGroupAnswer =new UpdateUserAttributeGroupAnswer();
+        updateUserAttributeGroupAnswer.setUser_attribute_group_answer(userAttributeGroupAnswer);
+
+        Map map2=updateUserAttributeGroupAnswer.request(defaultClient);
+        System.out.println("更新属性组回复");
+        for (Object o :map2.keySet()
+             ) {
+            System.out.println("map2");
+            System.out.println(o);
+            System.out.println(map2.get(o));
+        }
+
+        JSONObject json=JSONObject.parseObject(map2.get("user_attribute_group_answer").toString());
+        System.out.println(json);
+        JSONObject jsonObject=JSONObject.parseObject(json.get("answer").toString());
+
+
+        //4.删除属性组回复
+//        DeleteUserAttriButeGroupAnswer deleteUserAttriButeGroupAnswer=new DeleteUserAttriButeGroupAnswer();
+//        deleteUserAttriButeGroupAnswer.setId(jsonObject.get("id").toString());
+//        int code= deleteUserAttriButeGroupAnswer.request(defaultClient);
+//        System.out.println(code);
+
+    }
+
+
+
 }
